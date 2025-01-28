ab174e09ce9c4fc2e880986fc26524d0da40edb7
Boney <boney.abraham@ust.com>
2025-01-28 07:55:07 +0000

    updated new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
 import { WorkexperienceComponent } from './components/workexperience/workexperience.component';
 import { ContactmeComponent } from './components/contactme/contactme.component';
+import { HobbiesComponent } from './components/hobbies/hobbies.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'Home',pathMatch:'full'},
+    {path:'',redirectTo:'aboutme',pathMatch:'full'},
     {path:'aboutme',component:AboutmeComponent},
-    {path:'experience',component:WorkexperienceComponent},
+    {path:'hobbies',component:HobbiesComponent},
     {path:'contactme',component:ContactmeComponent}
 ];
